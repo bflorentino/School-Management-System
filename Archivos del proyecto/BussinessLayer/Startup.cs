1c2181ed4c84369531b40c3a55a6b35241d56e8d
Bryan <bryanmontero81@gmail.com>
2021-12-15 16:17:36 -0400

    Backend structure ready

--- Archivos del proyecto/BussinessLayer/Startup.cs
+++ Archivos del proyecto/BussinessLayer/Startup.cs
@@ -25,6 +25,10 @@ namespace BussinessLayer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => options.AddPolicy("AllowWebApp", builder =>
+                                                                                 builder.AllowAnyOrigin()   
+                                                                                 .AllowAnyHeader()
+                                                                                 .AllowAnyMethod()));
             services.AddControllers();
         }
 
@@ -35,7 +39,7 @@ namespace BussinessLayer
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors("AllowWebApp");
             app.UseHttpsRedirection();
 
             app.UseRouting();
